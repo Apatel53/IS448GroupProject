23e662877d25ce831f6ea66a395bd6537371b5d1
kc <kaelachen22@gmail.com>
2020-10-24 12:06:22 -0400

    nav

--- nav.js
+++ nav.js
@@ -1,7 +1,28 @@
 Vue.component('nav-bar', {
   template: `
   <nav>
-    <div>This is the nav bar</div>
+    <a href="./home">Home (logo here)</a>
+    <div style="float: right">
+      <a href="./menu">Menu</a>
+      <a href="./about">About</a>
+      <a href="./reviews">Reviews</a>
+    </div>
   </nav>
   `
+})
+
+Vue.component('nav-footer', {
+  template: `
+  <footer>
+    <a v-for="site of sites" :key="site" :href="'https://' + site + '.com'">
+      <img :src="'images/' + site + '.png'"/>
+    </a>
+    <p>© 2020 Idk Our Company. All rights reserved.</p>
+  </footer>
+  `,
+  data() {
+    return {
+      sites: ['facebook', 'instagram', 'youtube', 'twitter']
+    }
+  }
 })
